03b39fab3ea6d63d55a2ef8fbaad73aba4fda4d7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-05 10:40:31 -0400

    feat: Add form fields state, router, and useSignUp from @clerk/nextjs

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,4 +1,21 @@
+'use client'
+
+import { useSignUp } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
 const RegisterPage = () => {
+  const router = useRouter()
+  const { isLoaded, signUp, setActive } = useSignUp()
+
+  // states
+  const [email, setEmail] = useState<string>('')
+  const [firstName, setFirstName] = useState<string>('')
+  const [lastName, setLastName] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [pendingVerification, setPendingVerification] = useState<boolean>(false)
+  const [code, setCode] = useState<string>('')
+
   return <div>RegisterPage</div>
 }
 export default RegisterPage
