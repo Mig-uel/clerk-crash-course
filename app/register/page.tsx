7564a57f107d6354137169379d35368d819e9b9a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-05 11:01:25 -0400

    feat: Add form submission and email verification functionality to RegisterPage
    The RegisterPage component in page.tsx has been updated to include form submission functionality and email verification. Users can now submit the registration form and verify their email address. This change enhances the registration process and ensures a smooth user experience.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSignUp } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 const RegisterPage = () => {
   const router = useRouter()
@@ -16,6 +16,120 @@ const RegisterPage = () => {
   const [pendingVerification, setPendingVerification] = useState<boolean>(false)
   const [code, setCode] = useState<string>('')
 
-  return <div>RegisterPage</div>
+  // form submit
+  const handleSubmit = async (e: FormEvent) => {}
+
+  // verify user email code
+  const onPressVerify = async (e: FormEvent) => {}
+
+  return (
+    <div className='border p-5 rounded w-[500px]'>
+      <h1 onSubmit={handleSubmit} className='text-2xl mb-4'>
+        Register
+      </h1>
+      {!pendingVerification && (
+        <form action=''>
+          <div>
+            <label
+              htmlFor='firstName'
+              className='block mb-2 text-sm font-medium text-gray-900'
+            >
+              First Name
+            </label>
+            <input
+              type='text'
+              name='firstName'
+              id='firstName'
+              placeholder='John'
+              onChange={(e) => setFirstName(e.target.value)}
+              className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor='lastName'
+              className='block mb-2 text-sm font-medium text-gray-900'
+            >
+              Last Name
+            </label>
+            <input
+              type='text'
+              name='lastName'
+              id='lastName'
+              placeholder='Doe'
+              onChange={(e) => setLastName(e.target.value)}
+              className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor='email'
+              className='block mb-2 text-sm font-medium text-gray-900'
+            >
+              Email
+            </label>
+            <input
+              type='text'
+              name='email'
+              id='email'
+              placeholder='john@email.com'
+              onChange={(e) => setEmail(e.target.value)}
+              className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor='password'
+              className='block mb-2 text-sm font-medium text-gray-900'
+            >
+              Password
+            </label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              onChange={(e) => setPassword(e.target.value)}
+              className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5'
+              required
+            />
+          </div>
+
+          <button
+            type='submit'
+            className='w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 text-center'
+          >
+            Register
+          </button>
+        </form>
+      )}
+
+      {pendingVerification && (
+        <div>
+          <form className='space-y-4 md:space-y-6'>
+            <input
+              value={code}
+              className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5'
+              placeholder='Enter Verification Code...'
+              onChange={(e) => setCode(e.target.value)}
+            />
+
+            <button
+              type='submit'
+              onClick={onPressVerify}
+              className='w-full text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
+            >
+              Verify Email
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  )
 }
 export default RegisterPage
