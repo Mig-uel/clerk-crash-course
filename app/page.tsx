cefd2f1cea8786b531b9a17881d28ce2a1bb9da9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2024-09-05 09:59:07 -0400

    refactor: Update Home page with additional content
    The Home page in the page.tsx file has been updated to include a new heading and paragraph. This change adds more content to the Home page, providing additional information to users visiting the site.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className='text-2xl font-bold mb-5'>Home</h1>
+      <p className='mb-5'>This is a demo site for Clerk Authentication.</p>
     </>
   )
 }
